3769bba2f57edada13c73ce0c3787dfd3b4f3c93
Jake Miller <hillsighed@gmail.com>
2017-03-22 16:05:07 -0700

    cleaned up main.js

--- main.js
+++ main.js
@@ -1,15 +1,1 @@
 console.log('hi from main')
-
-var mercury = document.getElementById('mercury')
-console.log(mercury);
-
-// var t = 0;
-
-// function render() {
-//   t+= 0.01;
-//   console.log(t)
-//   requestAnimationFrame(render);
-//   mercury.setAttribute('position', '5 0 ' + t)
-// }
-
-// render();
