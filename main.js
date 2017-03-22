b49439a6ca4f2b698683ceea8c6b88d22727fa57
Jake Miller <hillsighed@gmail.com>
2017-03-22 15:02:29 -0700

    makes the sun the center of the universe, planets now orbit around it

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ console.log('hi from main')
 var mercury = document.getElementById('mercury')
 console.log(mercury);
 
-var t = 0;
+// var t = 0;
 
-function render() {
-  t+= 0.01;
-  requestAnimationFrame(render);
-  mercury.setAttribute('position', '3 ' +(Math.sin(t*2)+1)+' 5')
-}
+// function render() {
+//   t+= 0.01;
+//   console.log(t)
+//   requestAnimationFrame(render);
+//   mercury.setAttribute('position', '5 0 ' + t)
+// }
 
-render();
+// render();
