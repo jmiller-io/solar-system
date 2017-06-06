b9ab9618e3dc34b2b15eb81485c0cef50648bc54
Jake Miller <hillsighed@gmail.com>
2017-06-06 14:41:58 -0600

    adds ship doesnt associate with A-Frame entity yet

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 console.log('hi from main')
+
+// Obtain A-Frame Scene
+var scene = document.querySelector('a-scene').object3D
+
+var loader = new THREE.ObjectLoader();
+
+loader.load("./assets/star-wars-vader-tie-fighter.json", function ( obj ) {
+  scene.add(obj);
+
+  console.log(obj)
+  AFRAME.registerComponent('mythreejsthing', {
+  schema: obj,
+
+  init: function () {
+    var el = this.el;  // Entity.
+    var mythreejsobject = // ... Create three.js object.
+    el.setObject3D('mesh', mythreejsobject);
+  }
+});
+});
