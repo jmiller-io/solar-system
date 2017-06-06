378bb9a1c2611fae0b662291ca8d7668a3f8feac
Jake Miller <hillsighed@gmail.com>
2017-06-06 15:11:58 -0600

    adds spaceship to scene

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 console.log('hi from main')
 
-// Obtain A-Frame Scene
-var scene = document.querySelector('a-scene').object3D
+// // Obtain A-Frame Scene
+// var scene = document.querySelector('a-scene').object3D
 
-var loader = new THREE.ObjectLoader();
+// var loader = new THREE.ObjectLoader();
 
-loader.load("./assets/star-wars-vader-tie-fighter.json", function ( obj ) {
-  scene.add(obj);
+// loader.load("./assets/star-wars-vader-tie-fighter.json", function ( obj ) {
+//   scene.add(obj);
 
-  console.log(obj)
-  AFRAME.registerComponent('mythreejsthing', {
-  schema: obj,
+//   console.log(obj)
+//   AFRAME.registerComponent('mythreejsthing', {
+//   schema: obj,
 
-  init: function () {
-    var el = this.el;  // Entity.
-    var mythreejsobject = // ... Create three.js object.
-    el.setObject3D('mesh', mythreejsobject);
-  }
-});
-});
+//   init: function () {
+//     var el = this.el;  // Entity.
+//     var mythreejsobject = obj
+//     el.setObject3D('mesh', mythreejsobject);
+//   }
+// });
+// });
